5f85980ac770a288d0fc07a3e40925e33e40967a
Kunal Aggarwal <65971141+kunal270@users.noreply.github.com>
2021-10-02 14:33:09 +0530

    Add files via upload

---
+++ build/precache-manifest.25ff2681a31391ecf4d9f0f6f169234a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944e8bf257619eb8f02146873b5c4558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed528850691e2e3d190c",
+    "url": "/static/css/main.e508d319.chunk.css"
+  },
+  {
+    "revision": "e32a7dbabb3032a55caa",
+    "url": "/static/js/2.b4068277.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b4068277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed528850691e2e3d190c",
+    "url": "/static/js/main.666593ca.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
